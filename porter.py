2618e7ed594038fcf53d4b2091c6236754d3c122
anees50hertz <anees50hertz@gmail.com>
2024-06-13 00:16:17 +0200

    Stemming code applied for the Inverted model.

--- porter.py
+++ porter.py
@@ -1,5 +1,5 @@
 # Contains all functions related to the porter stemming algorithm.
-
+import re
 from document import Document
 
 
@@ -10,7 +10,25 @@ def get_measure(term: str) -> int:
     :return: Measure value m
     """
     # TODO: Implement this function. (PR03)
-    raise NotImplementedError('This function was not implemented yet.')
+    m = 0
+    vowel_found = False
+
+    for i in range(len(term)):
+        if is_vowel(term[i]):
+            vowel_found = True
+        else:
+            if vowel_found:
+                m += 1
+                vowel_found = False
+
+    return m
+
+
+def is_vowel(letter: str) -> bool:
+    """
+    Check if a letter is a vowel.
+    """
+    return letter in 'aeiou' or (letter == 'y' and len(letter) > 1 and not is_vowel(letter[-2]))
 
 
 def condition_v(stem: str) -> bool:
@@ -20,7 +38,7 @@ def condition_v(stem: str) -> bool:
     :return: True if the condition *v* holds
     """
     # TODO: Implement this function. (PR03)
-    raise NotImplementedError('This function was not implemented yet.')
+    return any(is_vowel(char) for char in stem)
 
 
 def condition_d(stem: str) -> bool:
@@ -30,7 +48,7 @@ def condition_d(stem: str) -> bool:
     :return: True if the condition *d holds
     """
     # TODO: Implement this function. (PR03)
-    raise NotImplementedError('This function was not implemented yet.')
+    return len(stem) > 1 and stem[-1] == stem[-2] and not is_vowel(stem[-1])
 
 
 def cond_o(stem: str) -> bool:
@@ -41,7 +59,14 @@ def cond_o(stem: str) -> bool:
     :return: True if the condition *o holds
     """
     # TODO: Implement this function. (PR03)
-    raise NotImplementedError('This function was not implemented yet.')
+    if len(stem) < 3:
+        return False
+    return (
+            not is_vowel(stem[-1]) and
+            is_vowel(stem[-2]) and
+            not is_vowel(stem[-3]) and
+            stem[-1] not in 'wxy'
+    )
 
 
 def stem_term(term: str) -> str:
@@ -52,7 +77,103 @@ def stem_term(term: str) -> str:
     """
     # TODO: Implement this function. (PR03)
     # Note: See the provided file "porter.txt" for information on how to implement it!
-    raise NotImplementedError('This function was not implemented yet.')
+    term = term.lower()
+    if len(term) <= 2:
+        return term
+
+    # Step 1a
+    if term.endswith('sses'):
+        term = term[:-2]
+    elif term.endswith('ies'):
+        term = term[:-2]
+    elif term.endswith('ss'):
+        term = term
+    elif term.endswith('s'):
+        term = term[:-1]
+
+    # Step 1b
+    if term.endswith('eed'):
+        if get_measure(term[:-3]) > 0:
+            term = term[:-1]
+    elif term.endswith('ed'):
+        stem = term[:-2]
+        if condition_v(stem):
+            term = step1b_helper(stem)
+    elif term.endswith('ing'):
+        stem = term[:-3]
+        if condition_v(stem):
+            term = step1b_helper(stem)
+
+    # Step 1c
+    if term.endswith('y'):
+        stem = term[:-1]
+        if condition_v(stem):
+            term = stem + 'i'
+
+    # Step 2
+    suffixes = {
+        'ational': 'ate', 'tional': 'tion', 'enci': 'ence', 'anci': 'ance',
+        'izer': 'ize', 'abli': 'able', 'alli': 'al', 'entli': 'ent',
+        'eli': 'e', 'ousli': 'ous', 'ization': 'ize', 'ation': 'ate',
+        'ator': 'ate', 'alism': 'al', 'iveness': 'ive', 'fulness': 'ful',
+        'ousness': 'ous', 'aliti': 'al', 'iviti': 'ive', 'biliti': 'ble',
+    }
+    for suffix, replacement in sorted(suffixes.items(), key=lambda x: -len(x[0])):
+        if term.endswith(suffix):
+            stem = term[:-len(suffix)]
+            if get_measure(stem) > 0:
+                term = stem + replacement
+                break
+
+    # Step 3
+    suffixes = {
+        'icate': 'ic', 'ative': '', 'alize': 'al', 'iciti': 'ic',
+        'ical': 'ic', 'ful': '', 'ness': ''
+    }
+    for suffix, replacement in sorted(suffixes.items(), key=lambda x: -len(x[0])):
+        if term.endswith(suffix):
+            stem = term[:-len(suffix)]
+            if get_measure(stem) > 0:
+                term = stem + replacement
+                break
+
+    # Step 4
+    suffixes = [
+        'al', 'ance', 'ence', 'er', 'ic', 'able', 'ible', 'ant', 'ement', 'ment', 'ent',
+        'ou', 'ism', 'ate', 'iti', 'ous', 'ive', 'ize'
+    ]
+    for suffix in sorted(suffixes, key=lambda x: -len(x)):
+        if term.endswith(suffix):
+            stem = term[:-len(suffix)]
+            if get_measure(stem) > 1:
+                term = stem
+                break
+    if term.endswith('ion'):
+        stem = term[:-3]
+        if get_measure(stem) > 1 and (stem.endswith('s') or stem.endswith('t')):
+            term = stem
+
+    # Step 5a
+    if term.endswith('e'):
+        stem = term[:-1]
+        if get_measure(stem) > 1 or (get_measure(stem) == 1 and not cond_o(stem)):
+            term = stem
+
+    # Step 5b
+    if get_measure(term) > 1 and condition_d(term) and term.endswith('l'):
+        term = term[:-1]
+
+    return term
+
+def step1b_helper(term: str) -> str:
+    if term.endswith(('at', 'bl', 'iz')):
+        return term + 'e'
+    if condition_d(term) and not term.endswith(('l', 's', 'z')):
+        return term[:-1]
+    if get_measure(term) == 1 and cond_o(term):
+        return term + 'e'
+    return term
+
 
 def stem_all_documents(collection: list[Document]):
     """
@@ -61,7 +182,8 @@ def stem_all_documents(collection: list[Document]):
     :param collection: Document collection to process
     """
     # TODO: Implement this function. (PR03)
-    raise NotImplementedError('This function was not implemented yet.')
+    for doc in collection:
+        doc.stemmed_terms = [stem_term(term) for term in doc.terms]
 
 
 def stem_query_terms(query: str) -> str:
@@ -71,4 +193,6 @@ def stem_query_terms(query: str) -> str:
     :return: Query with stemmed terms
     """
     # TODO: Implement this function. (PR03)
-    raise NotImplementedError('This function was not implemented yet.')
+    terms = query.split()
+    stemmed_terms = [stem_term(term) for term in terms]
+    return ' '.join(stemmed_terms)
